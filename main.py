5292f9c9eed4f96b0d9922f63a045920fb1cbf46
aRandomHumanoid <51519362+aRandomHumanoid@users.noreply.github.com>
2023-11-04 14:45:58 -0400

    wer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-# This is a sample Python script.
+from flask import Flask
+app = Flask(__name__)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-print("hello world")
+@app.route("/")
+def hello_world():
+    return "Hello, World!"

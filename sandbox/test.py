f96d3077ee275cdf9844b93c7e9295a0ffcba524
aRandomHumanoid <51519362+aRandomHumanoid@users.noreply.github.com>
2023-11-05 00:38:16 -0400

    sdfsdf
    i'm so tired

--- sandbox/test.py
+++ sandbox/test.py
@@ -1,8 +1,5 @@
-from flask import Flask, jsonify, request
-
 from marshmallow import Schema, fields
 
-app = Flask(__name__)
 class Post:
     def __init__(self, title, author, description, image, location):
         self.title = title
@@ -14,31 +11,18 @@ class Post:
     def __repr__(self):
         return "<Post(title={title.name!r})>".format(self=self)
 
+
 class PostSchema(Schema):
-    title = fields.Str()
-    author = fields.Str()
-    description = fields.Str()
-    image = fields.Str()
-    location = fields.Str()
+    title = fields.Str
+    author = fields.Str
+    description = fields.Str
+    image = fields.Str
+    location = fields.Str
 
     def make_post(self, data, **kwargs):
         return Post(**data)
 
-
 all_posts = [Post("hi", "me", "a nice place", "", "nowhere."), Post("heyy", "me", "a nice place", "", "nowhere.")]
 
-@app.route('/posts')
-def get_posts():
-    schema = PostSchema(many=True)
-    out = schema.dumps(all_posts)
-    return out
-
-
-@app.route('/posts', methods=['POST'])
-def add_post():
-    schema = PostSchema()
-    all_posts.append(schema.load(request.get_json()))
-    return '', 204
-
-if __name__ == "__main__":
-    app.run(debug=True)
+schema = PostSchema(many=True)
+print(schema.dump(all_posts))
